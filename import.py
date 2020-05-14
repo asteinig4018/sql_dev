bcdec840388421a96fe7dcf25e4e0b9f39080421
Alexander Steinig <asteinig@umich.edu>
2020-05-13 23:31:34 -0400

    finally imported my first data, import.py works and config.py does too

--- import.py
+++ import.py
@@ -6,7 +6,21 @@ from config import DATABASE_URI
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
 
-#engine = create_engine(,echo=True)
+engine = create_engine(DATABASE_URI,echo=True)
+db = scoped_session(sessionmaker(bind = engine))
 
-print(DATABASE_URI)
+def import_planes():
+    f = open("data/planes.dat")
+    reader = csv.reader(f)
+    for name, iata_code, icao_code in reader:
+        db.execute("INSERT INTO planes (name, iata_code, icao_code) VALUES (:name, :iata_code, :icao_code)",
+        {"name":name, "iata_code":iata_code, "icao_code":icao_code})
+        print(f"Added plane {name} with IATA {iata_code} and ICAO {icao_code}.")
 
+    db.commit()
+
+def main():
+    import_planes()
+
+if __name__ == "__main__":
+    main()
