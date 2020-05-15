e6bb65e634a2de02076245cc287393c02f328d0c
Alexander Steinig <asteinig@umich.edu>
2020-05-15 01:05:20 -0400

    databasing works new html page for submit endpoint databasing needs to be fixed to only show one of each city

--- application.py
+++ application.py
@@ -1,22 +1,26 @@
 from flask import Flask, request, render_template
 
-#from config import DATABASE_URI
+from config import DATABASE_URI
 
-#from sqlalchemy import create_engine
-#from sqlalchemy.orm import scoped_session, sessionmaker
+from sqlalchemy import create_engine
+from sqlalchemy.orm import scoped_session, sessionmaker
 
 app = Flask(__name__)
 
-#engine = create_engine(DATABASE_URI,echo=True)
-#db = scoped_session(sessionmaker(bind = engine))
+engine = create_engine(DATABASE_URI,echo=True)
+db = scoped_session(sessionmaker(bind = engine))
 
 @app.route("/")
 def index():
-    origins = list()
-    destinations = list()
-    airlines = list()
-    return render_template("index.html", origins=origins, destinations=destinations, airlines=airlines)
+    airports = db.execute("SELECT * FROM airports").fetchall()
+    airlines = db.execute("SELECT * FROM airlines").fetchall()
+    return render_template("index.html", airports=airports, airlines=airlines)
 
 @app.route("/check_flight", methods=["GET","POST"])
 def check_flight():
-    pass
+    if request.method == "POST":
+        origin = request.form.get("origin")
+        destination = request.form.get("destination")
+        airline = request.form.get("airline")
+
+    return render_template("check_flight.html")
